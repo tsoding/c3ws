421d67efbada1d020c6b669fcb72e66900a9b48b
rexim <reximkut@gmail.com>
2024-11-29 00:58:24 +0700

    Introduce example_echo_client

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 set -xe
 
 c3c compile -O5 -o example_send_client example_send_client.c3 example_config.c3 c3ws.c3
-c3c compile -O5 -o example_echo_server example_echo_server.c3 example_config.c3 c3ws.c3
+c3c compile -O5 -o example_echo_server example_echo_server.c3 example_echo.c3 example_config.c3 c3ws.c3
+c3c compile -O5 -o example_echo_client example_echo_client.c3 example_echo.c3 example_config.c3 c3ws.c3
