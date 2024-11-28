ec3218c26dac057d738ce6234bf0417ddb721ecd
rexim <reximkut@gmail.com>
2024-11-29 00:08:33 +0700

    Enable -O5 for example_client too

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 set -xe
 
 c3c compile -O5 -o example_client example_client.c3 example_config.c3 c3ws.c3
-c3c compile -o example_server example_server.c3 example_config.c3 c3ws.c3
+c3c compile -O5 -o example_server example_server.c3 example_config.c3 c3ws.c3
