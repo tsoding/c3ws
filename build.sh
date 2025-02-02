cc1c298e46178c1833f609198b830b44b8be413d
rexim <reximkut@gmail.com>
2025-02-02 18:39:52 +0700

    Remove early async server experiments

--- build.sh
+++ build.sh
@@ -5,6 +5,5 @@ set -xe
 mkdir -p build/
 
 c3c compile -O5 -o build/example_send_client examples/send_client.c3 c3ws.c3
-c3c compile -o build/example_async_server examples/async_server.c3 c3ws.c3 test/autobahn/config.c3
 c3c compile -O5 -o build/autobahn_echo_server test/autobahn/echo_server.c3 test/autobahn/echo.c3 test/autobahn/config.c3 c3ws.c3
 c3c compile -O5 -o build/autobahn_echo_client test/autobahn/echo_client.c3 test/autobahn/echo.c3 test/autobahn/config.c3 c3ws.c3
